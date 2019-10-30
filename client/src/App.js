b4f1fa9261837f4b72b2ed1234f5527c0894ff14
MelvinSam2000 <melvinsamxd@gmail.com>
2019-10-30 15:21:55 -0400

    Set up React

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './css/App.css'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Hello World
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App;
