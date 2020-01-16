69b26460083a1a9795ec71cfc0668bf8c814f8cf
MelvinSam2000 <melvinsamxd@gmail.com>
2020-01-16 18:26:59 -0500

    Set up deployment

--- server/app.js
+++ server/app.js
@@ -7,7 +7,15 @@ const app = express()
 const userRouter = require("./api/routes/user_routes")
 const authRouter = require("./api/routes/auth_routes")
 
+// JSON body parser
 app.use(bodyParser.json())
+
+// CORS enabled
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use("/user", userRouter)
 app.use("/auth", authRouter)
